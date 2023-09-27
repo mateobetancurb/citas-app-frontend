66db75d8421e24001622c20fad288564faf45b0a
Mateo <mateobetancurb@gmail.com>
2023-09-26 21:45:52 -0500

    services store

--- src/stores/services.js
+++ src/stores/services.js
@@ -3,14 +3,18 @@ import { defineStore } from "pinia";
 import ServicesAPI from "../api/ServicesAPI";
 
 export const useServicesStore = defineStore("services", () => {
+	const services = ref([]);
+
 	onMounted(async () => {
 		try {
 			const { data } = await ServicesAPI.all();
-			console.log(data);
+			services.value = data;
 		} catch (error) {
 			console.log(error);
 		}
 	});
 
-	return {};
+	return {
+		services,
+	};
 });
