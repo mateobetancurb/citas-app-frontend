51912139f3d91916336e8b45d8e60316a4bd1087
Mateo <mateobetancurb@gmail.com>
2023-09-26 21:46:07 -0500

    service item

---
+++ src/components/ServiceItem.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { formatCurrency } from "../helpers";
+defineProps({
+	service: {
+		type: Object,
+	},
+});
+</script>
+
+<template>
+	<div class="p-5 space-y-5 rounded-lg cursor-pointer bg-white">
+		<p class="text-xl font-extrabold">{{ service.name }}</p>
+		<p class="text-xl font-black text-blue-600">
+			{{ formatCurrency(service.price) }}
+		</p>
+	</div>
+</template>
