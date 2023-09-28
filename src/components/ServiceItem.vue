2d808870084d1e26408d75f2e36909691596ae23
Mateo <mateobetancurb@gmail.com>
2023-09-28 11:56:12 -0500

    seleccionando los servicios

--- src/components/ServiceItem.vue
+++ src/components/ServiceItem.vue
@@ -1,5 +1,9 @@
 <script setup>
 import { formatCurrency } from "../helpers";
+import { useAppointmentsStore } from "../stores/appointments";
+
+const appointmentsStore = useAppointmentsStore();
+
 defineProps({
 	service: {
 		type: Object,
@@ -8,9 +12,26 @@ defineProps({
 </script>
 
 <template>
-	<div class="p-5 space-y-5 rounded-lg cursor-pointer bg-white">
-		<p class="text-xl font-extrabold">{{ service.name }}</p>
-		<p class="text-xl font-black text-blue-600">
+	<div
+		@click="appointmentsStore.onServiceSelected(service)"
+		class="flex justify-between items-end flex-col p-5 space-y-5 rounded-lg cursor-pointer"
+		:class="
+			appointmentsStore.isServiceSelected(service._id)
+				? 'bg-blue-700 text-white'
+				: 'bg-white'
+		"
+	>
+		<p class="text-xl text-start font-extrabold">
+			{{ service.name }}
+		</p>
+		<p
+			class="text-2xl font-extrabold"
+			:class="
+				appointmentsStore.isServiceSelected(service._id)
+					? 'text-white'
+					: 'text-blue-900'
+			"
+		>
 			{{ formatCurrency(service.price) }}
 		</p>
 	</div>
