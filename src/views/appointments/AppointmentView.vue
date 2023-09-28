c252d210021cc0fb99a12778554e97c8a9ae0bc5
Mateo <mateobetancurb@gmail.com>
2023-09-28 13:01:13 -0500

    conditional rendering

--- src/views/appointments/AppointmentView.vue
+++ src/views/appointments/AppointmentView.vue
@@ -1,5 +1,33 @@
-<script setup></script>
+<script setup>
+import SelectedService from "../../components/SelectedService.vue";
+import { useAppointmentsStore } from "../../stores/appointments";
+
+const appointmentsStore = useAppointmentsStore();
+</script>
 
 <template>
-	<div><h1>Detalles de la cita</h1></div>
+	<h3
+		v-if="appointmentsStore.services.length === 0"
+		class="mt-24 text-white text-center text-xl font-extrabold"
+	>
+		Aún no has agregado servicios a tu cita
+	</h3>
+	<div v-if="appointmentsStore.services.length > 1">
+		<h2 class="text-3xl font-extrabold mt-10 text-white">
+			Detalles de tu cita
+		</h2>
+		<p class="text-white text-md mb-10">
+			Verifica la información y confirma tu cita
+		</p>
+		<h3 class="text-xl font-extrabold text-white mb-2">
+			Estos son los servicios que quieres agendar:
+		</h3>
+		<div class="grid gap-5">
+			<SelectedService
+				v-for="service in appointmentsStore.services"
+				:key="service._id"
+				:service="service"
+			/>
+		</div>
+	</div>
 </template>
