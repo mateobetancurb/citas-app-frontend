3bf0d85d1ced3683f7e5196f1fbf4d12173061d6
Mateo <mateobetancurb@gmail.com>
2023-09-28 14:25:55 -0500

    total a pagar

--- src/views/appointments/AppointmentView.vue
+++ src/views/appointments/AppointmentView.vue
@@ -1,13 +1,14 @@
 <script setup>
 import SelectedService from "../../components/SelectedService.vue";
 import { useAppointmentsStore } from "../../stores/appointments";
+import { formatCurrency } from "../../helpers";
 
 const appointmentsStore = useAppointmentsStore();
 </script>
 
 <template>
 	<h3
-		v-if="appointmentsStore.services.length === 0"
+		v-if="appointmentsStore.noServicesSelected"
 		class="mt-24 text-white text-center text-xl font-extrabold"
 	>
 		Aún no has agregado servicios a tu cita
@@ -28,6 +29,12 @@ const appointmentsStore = useAppointmentsStore();
 				:key="service._id"
 				:service="service"
 			/>
+			<h2 class="text-right text-xl text-white">
+				Total a pagar:
+				<span class="font-black text-2xl">
+					{{ formatCurrency(appointmentsStore.totalAmount) }}
+				</span>
+			</h2>
 		</div>
 	</div>
 </template>
