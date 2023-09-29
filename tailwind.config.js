654424105688e25a678a79ee4321b8a5acd5d92a
Mateo <mateobetancurb@gmail.com>
2023-09-29 13:21:28 -0500

    date picker config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-	content: ["./index.html", "./src/**/*.{vue,js}"],
+	content: [
+		"./index.html",
+		"./src/**/*.{vue,js}",
+		"./node_modules/vue-tailwind-datepicker/**/*.js",
+	],
 	theme: {
 		extend: {
 			backgroundImage: {
 				app: "url('/img/home.jpg')",
+			},
+			colors: {
+				"vtd-primary": colors.blue,
 			},
 		},
 	},
