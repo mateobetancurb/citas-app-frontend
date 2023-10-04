8e2e55edf5a1c4f7a50d0f243d25f3629bf28de4
Mateo <mateobetancurb@gmail.com>
2023-10-04 13:21:53 -0500

    vue-toast config

--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { plugin, defaultConfig } from "@formkit/vue";
+import { useToast } from "vue-toast-notification";
 import config from "../formkit.config";
 
 import App from "./App.vue";
 import router from "./router";
 
+import "vue-toast-notification/dist/theme-bootstrap.css";
+
+const $toast = useToast({
+	position: "top-right",
+	pauseOnHover: true,
+	queue: true,
+});
+
 const app = createApp(App);
 
+app.provide("toast", $toast);
 app.use(createPinia());
 app.use(plugin, defaultConfig(config));
 app.use(router);
