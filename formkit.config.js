a4fdc00d4f612ab40aaa8f4eb9030f762d32133e
Mateo <mateobetancurb@gmail.com>
2023-10-04 10:03:49 -0500

    formkit styles

--- formkit.config.js
+++ formkit.config.js
@@ -7,9 +7,9 @@ const config = {
 				wrapper: "space-y-2 mb-3",
 				message:
 					"bg-red-300 text-red-800 text-center text-sm font-bold p-2 my-5 rounded-lg",
-				label: "block mb-1 font-bold text-lg text-white",
+				label: "block mb-1 font-bold text-md text-white",
 				input:
-					"w-full p-3 border border-gray-300 rounded-lg text-gray-700 placeholder-gray-400 ",
+					"w-full p-3 border border-gray-300 rounded-lg text-gray-700 placeholder-gray-400",
 			},
 			submit: {
 				input:
