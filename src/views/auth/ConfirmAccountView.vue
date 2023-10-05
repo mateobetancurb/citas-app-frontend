1b88fb458462d7fe1e8368d9a7b758dbfbdb6452
Mateo <mateobetancurb@gmail.com>
2023-10-04 20:01:47 -0500

    view account confirm

--- src/views/auth/ConfirmAccountView.vue
+++ src/views/auth/ConfirmAccountView.vue
@@ -1,8 +1,53 @@
-<script setup></script>
+<script setup>
+import { onMounted, inject, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import AuthAPI from "../../api/AuthAPI";
+
+const route = useRoute();
+const router = useRouter();
+const { token } = route.params;
+const toast = inject("toast");
+const secondsToRedirect = ref(7);
+const isTokenValid = ref(true);
+
+const countdown = ref(secondsToRedirect.value);
+let countdownInterval;
+
+onMounted(async () => {
+	try {
+		const { data } = await AuthAPI.verifyAccount(token);
+		toast.open({
+			message: data.msg,
+			type: "success",
+			duration: 6000,
+		});
+
+		countdownInterval = setInterval(() => {
+			countdown.value -= 1;
+			if (countdown.value === 0) {
+				clearInterval(countdownInterval);
+				router.push({ name: "login" });
+			}
+		}, 1000);
+	} catch (error) {
+		console.log(error);
+		toast.open({
+			message: error.response.data.msg,
+			type: "error",
+			duration: 6000,
+		});
+
+		isTokenValid.value = false;
+	}
+});
+</script>
 
 <template>
-	<div>
-		<h1>desde confirmar usuario</h1>
+	<div v-if="isTokenValid">
+		<p class="text-xl font-extrabold text-white text-center mt-32">
+			En <span class="text-4xl">{{ countdown }}</span> segundos podrás iniciar
+			sesión
+		</p>
+		<RouterView />
 	</div>
-	<RouterView />
 </template>
