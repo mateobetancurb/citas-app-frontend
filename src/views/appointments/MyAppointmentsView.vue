098420480a12bf7ea32bf137b8d6e74c49d0f1c0
Mateo <mateobetancurb@gmail.com>
2023-10-07 13:31:25 -0500

    my appointments view

---
+++ src/views/appointments/MyAppointmentsView.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+	<h2 class="text-4xl font-extrabold text-white mt-10">Mis citas</h2>
+	<p class="text-white text-lg mt-5">
+		Aquí podrás administrar tus próximas citas
+	</p>
+</template>
