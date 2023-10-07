520d9761450e33c372d865163a0b4817f2cd663f
Mateo <mateobetancurb@gmail.com>
2023-10-07 16:24:27 -0500

    auth user - pinia store

--- src/views/appointments/MyAppointmentsView.vue
+++ src/views/appointments/MyAppointmentsView.vue
@@ -1,4 +1,7 @@
-<script setup></script>
+<script setup>
+import { useAuthUserStore } from "../../stores/authUser";
+const user = useAuthUserStore();
+</script>
 
 <template>
 	<h2 class="text-4xl font-extrabold text-white mt-10">Mis citas</h2>
