7f1bfc759b9310a4f6845ac4226eee6f2a67000a
Mateo <mateobetancurb@gmail.com>
2023-10-08 12:49:47 -0500

    interceptors config - axios

--- src/api/AppointmentAPI.js
+++ src/api/AppointmentAPI.js
@@ -4,10 +4,9 @@ export default {
 	create(data) {
 		const token = localStorage.getItem("AUTH_TOKEN");
 
-		return api.post("/appointments/create-appointment", data, {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		});
+		return api.post("/appointments/create-appointment", data);
+	},
+	getByDate(date) {
+		return api.get(`/appointments/create-appointment?date=${date}`, data);
 	},
 };
