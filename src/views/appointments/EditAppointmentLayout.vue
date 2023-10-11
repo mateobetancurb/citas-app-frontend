40c2fcd4c046921938145049c6bf0b08f6270d27
Mateo <mateobetancurb@gmail.com>
2023-10-10 20:15:51 -0500

    edit appointment

---
+++ src/views/appointments/EditAppointmentLayout.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
+
+<template>
+	<nav class="my-5 flex gap-3">
+		<RouterLink
+			:to="{ name: 'edit-appointment' }"
+			class="flex-1 text-center p-3 font-extrabold transition-colors hover:bg-blue-600 hover:text-white rounded-xl"
+			:class="
+				route.name === 'edit-appointment'
+					? 'bg-blue-700 text-white'
+					: 'bg-white text-blue-500'
+			"
+		>
+			Servicios
+		</RouterLink>
+		<RouterLink
+			:to="{ name: 'edit-appointment-details' }"
+			class="flex-1 text-center p-3 font-extrabold transition-colors hover:bg-blue-600 hover:text-white rounded-xl"
+			:class="
+				route.name === 'edit-appointment-details'
+					? 'bg-blue-700 text-white'
+					: 'bg-white text-blue-500'
+			"
+		>
+			Cita y Resumen
+		</RouterLink>
+	</nav>
+	<RouterView />
+</template>
