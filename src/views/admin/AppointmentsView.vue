dea35c8a8faec55885cf216720c725c9bb66cf74
Mateo <mateobetancurb@gmail.com>
2023-10-15 12:22:09 -0500

    vista citas - admin

---
+++ src/views/admin/AppointmentsView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useAuthUserStore } from "../../stores/authUser";
+
+const userStore = useAuthUserStore();
+</script>
+
+<template>
+	<h2
+		v-if="userStore.noAppointments === false"
+		class="text-white text-lg mt-10 font-bold text-center"
+	>
+		Aquí podrás administrar tus próximas citas
+	</h2>
+
+	<p v-if="userStore.isLoading" class="text-white text-center text.2xl mt-5">
+		Cargando...
+	</p>
+	<div v-else>
+		<p
+			v-if="userStore.noAppointments"
+			class="text-white text-lg text-center mt-10"
+		>
+			Aún no hay citas agendadas
+		</p>
+	</div>
+</template>
