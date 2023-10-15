11cba7c69fab6602748c8304a897af3db743b450
Mateo <mateobetancurb@gmail.com>
2023-10-15 12:01:06 -0500

    routing - admin

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,11 @@ const router = createRouter({
 			component: HomeView,
 		},
 		{
+			path: "/admin",
+			name: "admin",
+			component: () => import("../views/admin/AdminLayout.vue"),
+		},
+		{
 			path: "/reservaciones",
 			name: "appointments",
 			component: AppointmentsLayout,
@@ -102,7 +107,7 @@ router.beforeEach(async (to, from, next) => {
 		try {
 			const { data } = await AuthAPI.auth();
 			if (data.user.admin) {
-				next("/admin");
+				next({ name: "admin" });
 			} else {
 				next();
 			}
