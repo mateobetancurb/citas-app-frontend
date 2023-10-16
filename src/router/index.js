04c87c938810b78aa4c81bcdcd059da0179d595c
Mateo <mateobetancurb@gmail.com>
2023-10-16 18:33:44 -0500

    router app

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 import AppointmentsLayout from "../views/appointments/AppointmentsLayout.vue";
 import AuthAPI from "../api/AuthAPI";
 
@@ -9,7 +8,7 @@ const router = createRouter({
 		{
 			path: "/",
 			name: "home",
-			component: HomeView,
+			component: () => import("../views/HomeView.vue"),
 		},
 		{
 			path: "/admin",
