8fbfdf9abb955d4a063969ff379a7f9ce184bbfe
Mateo <mateobetancurb@gmail.com>
2023-10-15 19:35:10 -0500

    auth api

--- src/api/AuthAPI.js
+++ src/api/AuthAPI.js
@@ -8,13 +8,13 @@ export default {
 		return api.get(`/auth/verify/${token}`);
 	},
 	login(data) {
-		return api.post("auth/login", data);
+		return api.post("api/auth/login", data);
 	},
 	auth() {
-		return api.get("/auth/user");
+		return api.get("api/auth/user");
 	},
 	isAdmin() {
-		return api.get("/auth/admin");
+		return api.get("api/auth/admin");
 	},
 	forgotPassword(data) {
 		return api.post("/auth/forgot-password", data);
